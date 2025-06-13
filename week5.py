cb379de84aad687dcade565f88354d440972cb68
Blair <mawubablair@gmail.com>
2025-06-13 21:52:36 +0300

    Add files via upload

---
+++ week5.py
@@ -0,0 +1,23 @@
+print("wellcame to Jipra elites school 2025 greading system")
+marks =int(input("please enter marks between 0 and 100:"))
+if marks >=90 and marks <=100:
+    print("D1")
+elif marks >=80 and marks <90:
+    print("D2")
+elif marks >=70 and marks <80:
+    print("D3")
+elif marks >=60 and marks <70:
+    print("C4")
+elif marks >=50 and marks <60:
+    print("C5")
+elif marks >=40 and marks <50:
+    print("C6")
+elif marks >=30 and marks <40:
+    print("P7")
+elif marks >=20 and marks <30:
+    print("P8")
+elif marks >=0 and marks <20:
+    print("F9")
+else:
+    print("lnvalid, plesase enter number between 0 and 100 ") 
+
