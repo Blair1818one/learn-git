d726ab53d3a8327858f90a6a2c32f3388dca78e0
Blair <mawubablair@gmail.com>
2025-06-15 23:51:18 +0300

    Add files via upload

---
+++ pratise.py
@@ -0,0 +1,16 @@
+# Assign the 10 to the variable 'age'
+age = 10
+print(f"The value of age is: {age}")
+
+# Assign the string "python" to the variable 'langulage'
+langulage = "python"
+print(F"the value of lungulage is: {langulage}")
+
+# compare if the value of 'age' is equal to 10
+age = 10
+is_age_ten = (age ==10)
+print(f"Is age equal to 10? {is_age_ten}")
+
+# compare if the pthon 'langulage' is equal to "python"
+is_langualage_python = (langulage =="python")
+print(f"Is langulage equal to 'python'? {is_langualage_python}")
