c6d81d87ffe5358f789abc497d6efb5cbadb6530
Blair <mawubablair@gmail.com>
2025-06-19 09:37:56 +0300

    Add files via upload

---
+++ Week6.py
@@ -0,0 +1,12 @@
+User_name = "john Doe"
+print(f"Good morning,{'user_name'} 👀👏")
+
+user_name = "pop name"
+print(f"good morning,{user_name} 👍")
+
+sum = 5 + 9
+print(f"good morning,{sum}")
+
+def square_of_numbers(X):
+    resut =X *X
+print('resut_of-X')          
